af1955e7ba644f345c0902a61df4e29f84f0d5ff
Rom <haedo.r333@gmail.com>
2025-05-01 08:44:05 -0300

    BORRE TODO PORQUE PARA APRENDER NO HAY QUE COPIARSE

--- GUIA-4-RECURSION/ej5.hs
+++
@@ -1,6 +0,0 @@
-medioFact :: Int -> Int
-medioFact n | n == 0 = 1
-            | n == 1 = 1
-            | otherwise = n * medioFact (n-2)
-
---es como el factorial pero aca hay dos casos base porque si voy restando de a dos llego al 1 o al 0
